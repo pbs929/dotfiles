de8acfc2a0d4cff5fa8bc911b512594aa8e8d1f9
Phillip Schafer <phillip.schafer@ironnetcybersecurity.com>
2018-07-26 10:31:42 -0400

    updates during new machine setup

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 My dotfiles, based mostly on those of `mathiasbynens`.
 
-To install, run all the shell scripts, particularly `bootstrap.sh`. 
+To install, run all the shell scripts, particularly `bootstrap.sh`.
+
+To get the solarized dark theme for Terminal on OSX, open terminal preferences and import the `init/Solarized Dark.terminal` file.
--- osx.sh
+++ osx.sh
@@ -5,10 +5,10 @@
 osascript -e 'tell application "System Preferences" to quit'
 
 # Ask for the administrator password upfront
-sudo -v
+# sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until `.macos` has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+# while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 
 ###############################################################################
@@ -16,7 +16,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 ###############################################################################
 
 # Disable the sound effects on boot
-sudo nvram SystemAudioVolume=" "
+# sudo nvram SystemAudioVolume=" "
 
 # Set the keyboard repeat rate
 defaults write NSGlobalDomain KeyRepeat -int 2
@@ -55,5 +55,5 @@ defaults write com.apple.screencapture location -string "${HOME}/Desktop/screens
 defaults write com.apple.terminal StringEncodings -array 4
 
 # Use Solarized Dark terminal theme
-open '$HOME/init/Solarized\ Dark.terminal'
-set default settings to settings set "Solarized Dark"
+# open "$HOME/init/Solarized\ Dark.terminal"
+# set default settings to settings set "Solarized Dark"
